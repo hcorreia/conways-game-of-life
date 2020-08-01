32befd27ed8e6c2f61453db97d091d0ce82cade3
Humberto Correia <correia.humberto@gmail.com>
2020-08-01 22:36:36 +0100

    perf: use single Vec insted of one per line

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,20 @@ const DEAD_CHAR: &str = "\u{2591}\u{2591}";
 pub struct BoardState {
     width: i32,
     height: i32,
-    cells: Vec<Vec<u8>>,
+    cells: Vec<u8>,
 }
 
 impl BoardState {
     pub fn new(width: i32, height: i32) -> BoardState {
-        let cells = vec![vec![DEAD; width as usize]; height as usize];
+        let cells = vec![DEAD; (width*height) as usize];
 
         return BoardState {width, height, cells};
-        // let state = BoardState {width, height, cells};
-
-
-        // return ref_state;
     }
 
     fn get_index(&self, x: i32, y: i32) -> u8 {
         if x >= 0 && y >= 0 && x < self.width && y < self.height {
             // println!("deltas {:?}", (x, y));
-            return self.cells[y as usize][x as usize];
+            return self.cells[(y*self.width + x) as usize];
         }
         return DEAD;
     }
@@ -49,27 +45,30 @@ impl BoardState {
 
     pub fn gen_next(&mut self) {
         // let mut next_state = self.cells.clone();
-        let mut next_state = vec![vec![DEAD; self.width as usize]; self.height as usize];
+        let mut next_state = vec![DEAD; (self.width*self.height) as usize];
         let mut neighbors: u8;
+        let mut x;
+        let mut y;
 
-        for y in 0..self.height {
-            for x in 0..self.width {
-                neighbors = self.get_neighbors(x, y);
+        for i in 0..(self.height*self.width) {
+            x = i % self.width;
+            y = i / self.width;
 
-                if self.cells[y as usize][x as usize] == LIVE {
-                    if neighbors <= 1 {
-                        next_state[y as usize][x as usize] = DEAD;
-                    } else if neighbors >= 4 {
-                        next_state[y as usize][x as usize] = DEAD;
-                    } else {
-                        next_state[y as usize][x as usize] = LIVE;
-                    }
+            neighbors = self.get_neighbors(x, y);
+
+            if self.cells[i as usize] == LIVE {
+                if neighbors <= 1 {
+                    next_state[i as usize] = DEAD;
+                } else if neighbors >= 4 {
+                    next_state[i as usize] = DEAD;
                 } else {
-                    if neighbors == 3 {
-                        next_state[y as usize][x as usize] = LIVE;
-                    } else {
-                        next_state[y as usize][x as usize] = DEAD;
-                    }
+                    next_state[i as usize] = LIVE;
+                }
+            } else {
+                if neighbors == 3 {
+                    next_state[i as usize] = LIVE;
+                } else {
+                    next_state[i as usize] = DEAD;
                 }
             }
         }
@@ -86,50 +85,54 @@ fn init_state_empty(width: i32, height: i32) -> BoardState {
     return state;
 }
 
+
 fn init_state_random(width: i32, height: i32) -> BoardState {
     let mut state = BoardState::new(width, height);
 
-    for y in 0..state.height {
-        for x in 0..state.width {
-            state.cells[y as usize][x as usize] = random::<bool>() as u8;
-        }
+    for i in 0..(state.height*state.width) {
+        state.cells[i as usize] = random::<bool>() as u8;
     }
 
     return state;
 }
+
 
 fn init_state_glider(width: i32, height: i32) -> BoardState {
     let mut state = init_state_empty(width, height);
 
-    state.cells[2][0] = LIVE;
-    state.cells[2][1] = LIVE;
-    state.cells[2][2] = LIVE;
-    state.cells[1][2] = LIVE;
-    state.cells[0][1] = LIVE;
+    state.cells[(2*width + 0) as usize] = LIVE;
+    state.cells[(2*width + 1) as usize] = LIVE;
+    state.cells[(2*width + 2) as usize] = LIVE;
+    state.cells[(1*width + 2) as usize] = LIVE;
+    state.cells[(0*width + 1) as usize] = LIVE;
 
     return state;
 }
 
+
 fn draw(state: &BoardState) {
+    let mut line = String::from("");
+
     println!("");
     println!("");
     println!("");
     println!("");
 
-    for row in &state.cells {
-        let mut line = String::from("");
+    for (i, cell) in state.cells.iter().enumerate() {
 
-        for cel in row {
-            if *cel == LIVE {
-                line.push_str(LIVE_CHAR);
-            } else {
-                line.push_str(DEAD_CHAR);
-            }
+        if *cell == LIVE {
+            line.push_str(LIVE_CHAR);
+        } else {
+            line.push_str(DEAD_CHAR);
         }
-        println!("{}", line);
-        line.clear();
+
+        if  (i as i32  + 1) % state.width == 0 {
+            println!("{}", line);
+            line.clear();
+        }
     }
 }
+
 
 fn life(width: i32, height: i32, limit: i64, wait: u64, debug: bool) {
     let sleep_time = time::Duration::from_millis(wait);
@@ -142,9 +145,11 @@ fn life(width: i32, height: i32, limit: i64, wait: u64, debug: bool) {
     if debug {
         now = time::SystemTime::now();
         state = init_state_random(width, height);
+        // state = init_state_glider(width, height);
         println!("Tick 1 ! {:?}", now.elapsed());
     } else {
         state = init_state_random(width, height);
+        // state = init_state_glider(width, height);
         draw(&state);
     }
 
@@ -162,10 +167,12 @@ fn life(width: i32, height: i32, limit: i64, wait: u64, debug: bool) {
     }
 }
 
+
 fn main() {
     life(
         // width, height
         // 80, 38,
+        // 5, 10,
         2_000, 2_000,
         // max iter
         10,
